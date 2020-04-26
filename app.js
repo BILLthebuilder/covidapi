b6f74346272d6035aad52af179a5f7b29fcb3f28
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-04-26 15:45:01 +0300

    Revert "refactor app entry point"
    This reverts commit 9212fa4f9057fa387705b8e51e9fd5f80ef3fe25.

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const routes = require('./api/v1/routes/routes');
+const db = require('./api/v1/db/db');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
+// routes for the app
+const ApiV1 = require('./api/v1/routes/routes');
+const usersRoute = require('./api/v1/routes/usersRouter');
+const DoctorsRoute = require('./api/v1/routes/doctorsRouter');
+const adminRoute = require('./api/v1/routes/adminRouter');
+const homeRouter = require('./api/v1/routes/homeRouter');
+
+app.use('/', homeRouter);
+app.use('/api/v1', ApiV1);
+app.use('/admin', adminRoute);
+app.use('/users', usersRoute);
+app.use('/doctors', DoctorsRoute);
 
 module.exports = app;
