a2750a850df720dbebd4a59f2f1714821783b21e
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-04-26 15:44:22 +0300

    Revert "refactor routes file"
    This reverts commit 9bf234525523197d3560b74294679701464dc586.

--- api/v1/routes/routes.js
+++ api/v1/routes/routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const Tipscontroller = require('../controllers/tipsController');
 
-// Routes for all the tips here
-router.post('/api/v1/tip', Tipscontroller.create);
-router.get('/api/v1/tips', Tipscontroller.getAll);
+// Define the routes here
+router.get('/', (req, res) => {
+    res.json('Api V1 Working 🚀');
+});
+
 module.exports = router;
